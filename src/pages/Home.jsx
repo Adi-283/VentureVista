0cac4f61ffa56504a6ecdc154d72e4027c164421
Prince Patel <princejanakbhai@gmail.com>
2024-01-29 18:00:13 -0500

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div><h1>Your File</h1>Home new add</div>
+        <div><h1>Your File</h1>Home new add   <p> venturevista</p></div>
     )
 }
 
