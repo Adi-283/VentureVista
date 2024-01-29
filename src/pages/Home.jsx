27d2c39f40d7c67656ba31a9cc6d54238425df11
Pratik Patel <patelpratik10899@gmail.com>
2024-01-29 18:01:46 -0500

    Meet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div><h1>Your File</h1>Home new add   <p> venturevista</p></div>
+        <div><h1>Your File</h1>Home new add   <p> Hi baby venturevista</p></div>
     )
 }
 
