0839f81d7514ac81e62e4a6e737c16d351011538
Meet Patel <meetpatel191120@gmail.com>
2024-02-01 13:57:35 -0500

    Edit

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import CommonSection from "../shared/CommonSection";
+import '../styles/tour.css';
+import TourCard from './../shared/TourCard';
+import SearchBar from './../shared/SearchBar';
+import NewsLetter from './../shared/Newsletter';
+import tourData from '../assets/data/tours';
+import { Col, Container, Row } from "reactstrap";
 
 const Tours = () => {
     return (
-        <div>Tours</div>
-    )
+    <>
+    < CommonSection title={'All Tours'}/>
+    <section>
+        <Container>
+            <Row>
+                <SearchBar />
+            </Row>
+        </Container>
+        <Container>
+            <Row>
+                {
+                    tourData ?.map(tour => 
+                    <Col lg='3' key={tour.id}>
+                        {"  "}
+                    <TourCard tour={tour} />
+                    </Col>)
+                }
+            </Row>
+        </Container>
+    </section>
+    </>
+    );
 }
 
 export default Tours;
