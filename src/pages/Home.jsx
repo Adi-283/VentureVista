b45bcb590a62a179e991a9c7908ec9f7593511c0
Pratik Patel <patelpratik10899@gmail.com>
2024-01-31 21:15:10 -0500

    Addition of Experience Gallery and slider in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,17 @@
 import React from "react";
 import '../styles/home.css'
 import { Container, Row, Col } from "reactstrap";
-import heroImg from '../assets/images/heroimg1.jpg'
-import heroImg02 from '../assets/images/heroimg2.jpg'
+import heroImg from '../assets/images/heroimg1.jpg';
+import heroImg02 from '../assets/images/heroimg2.jpg';
 // import heroVideo from '../assets/images/hero-video.mp4'
-import worldImg from '../assets/images/world.jpg'
+import worldImg from '../assets/images/world.jpg';
+import experienceImg from '../assets/images/heroimg1.jpg';
 import Subtitle from "../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/featured-tours/FeaturedTourList";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+import Testimonials from "../components/Testimonial/Testimonials";
 
 
 const Home = () => {
@@ -80,6 +83,82 @@ const Home = () => {
             </Container>
         </section>
         { /* ======================= Featured Tour section end ================= */}
+        { /* ======================= Experience section start ================= */}
+
+        <section>
+            <Container>
+                <Row>
+                    <Col lg="6">
+                        <div className="experience_content">
+                            <Subtitle subtitle={'Experience'}/>
+                            <h2>With our all experience <br /> we will serve you</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quis. Voluptatum fuga reprehenderit autem nisi eveniet rem labore consectetur aut possimus ab dicta, illo eos optio a iusto exercitationem! Doloribus.</p>
+                        </div>
+
+                        <div className="counter_wrapper d-flex align-items-center gap-5">
+                            <div className="counter__box">
+                                <span>12k+</span>
+                                <h6>Successful Trip</h6>
+                            </div>
+                            <div className="counter__box">
+                                <span>2k+</span>
+                                <h6>Regular Clients</h6>
+                            </div>
+                            <div className="counter__box">
+                                <span>15</span>
+                                <h6>Years Experience</h6>
+                            </div>
+                        </div>
+
+
+                    </Col>
+                    <Col lg="6">
+                        <div className="experience__img">
+                            <img src={experienceImg} alt="" />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+
+
+        { /* ======================= Experience section end ================= */}
+
+        { /* ======================= gallery section Start ================= */}
+        <section>
+            <Container>
+                <Row>
+                    <Col lg="12">
+                        <Subtitle subtitle={"Gallery"} />
+                        <h2 className="gallery__title">
+                            Visit our customer's tour gallery
+                        </h2>
+                    </Col>
+                    <Col lg="12">
+                        <MasonryImagesGallery />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+        { /* ======================= gallery section end ================= */}
+
+        { /* ======================= testimonial section Start ================= */}
+        <section>
+            <Container>
+                <Row>
+                    <Col lg = '12'>
+                        <Subtitle subtitle={'Fans Love'} />
+                        <h2 className="testimonial__title">What our Fans say about us</h2>
+                    </Col>
+                    <Col lg='12'>
+                        <Testimonials />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+        { /* ======================= testimonial section end ================= */}
     </>
     
 };
