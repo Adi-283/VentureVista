a19ad374fdc979193c267f49dcd2f2ab9c139789
Pratik Patel <patelpratik10899@gmail.com>
2024-02-03 23:22:41 -0500

    Login and Register

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -9,12 +9,12 @@ const ThankYou = () => {
 
         <Container>
             <Row>
-                <Col lg='12' className="pt-5 text-centre">
+                <Col lg='12' className="pt-5 text-center">
                     <div className="thank__you">
                         <span><i class="ri-checkbox-circle-line"></i></span>
                         <h1 className="mb-3 fw-semibold">Thank You</h1>
                         <h3 className="mb-4">your tour is booked.</h3 >
-                        <Button className="btn_primary_btn_w-25">
+                        <Button className="btn primary__btn w-25">
                             <Link to="/home">Back to Home</Link>
                         </Button>
                     </div>
