048fd046aeb6733c4e431386e687aed636f7c4f4
Prince Patel <princejanakbhai@gmail.com>
2024-02-10 13:02:36 -0500

    add auth login reg beckend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,24 +1,43 @@
 import React,  {useState}from "react";
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/login.css';
 import registerImg from '../assets/images/login.jpg'
 import userIcon from '../assets/images/user.jpg'
+
+import { validateRegistration, saveCredentialsToLocalStorage } from "../utils/auth"
+
 const Register = () => {
-
+    const navigate = useNavigate()
     const [credentials, setCredentials] = useState({
-        userName:undefined,
-       email:undefined,
-       password:undefined
-    })
+        username: '',
+        email: '',
+        password: ''
+    });
 
-    const handleChange = e => {
-        setCredentials(prev => ({ ...prev, [e.target.id]: e.target.value }))
+    const [error, setError] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+        setError((prev) => ({ ...prev, [e.target.id]: '' }));
     };
 
+    const handleClick = (e) => {
+        e.preventDefault();
 
-    const handleClick = e =>{
-        e.preventDefault()
+        const validationErrors = validateRegistration(credentials.username, credentials.email, credentials.password);
+
+        if (validationErrors.username || validationErrors.email || validationErrors.password) {
+            setError(validationErrors);
+            return;
+        }
+        saveCredentialsToLocalStorage(credentials);
+
+        navigate('/login');
     };
     return (
 
@@ -36,18 +55,21 @@ const Register = () => {
                         <img src =  {userIcon} alt="" />
                          </div>
                          <h2>Register</h2>
-                         <Form onSubmit={handleClick}>
-                            <FormGroup>
-                                <input type ="text" placeholder="Username" required id="username" onChange={handleChange}/>
-                            </FormGroup>
-                            <FormGroup>
-                                <input type ="email" placeholder="Email" required id="email" onChange={handleChange}/>
-                            </FormGroup>
-                            <FormGroup>
-                                <input type ="password" placeholder="Password" required id="password" onChange={handleChange}/>
-                            </FormGroup>
-                            <button className="btn secondary__btn auth__btn" type= "submit">Create Account</button>
-                            <p> Already have any account?  <Link to= '/login'>Login</Link> </p>
+                         <Form onSubmit={handleClick} noValidate>
+                                    <FormGroup>
+                                        <input type="text" placeholder="Username" required id="username" onChange={handleChange} />
+                                        {error.username && <p className="error-message">{error.username}</p>}
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <input type="email" placeholder="Email" required id="email" onChange={handleChange} />
+                                        {error.email && <p className="error-message">{error.email}</p>}
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <input type="password" placeholder="Password" required id="password" onChange={handleChange} />
+                                        {error.password && <p className="error-message">{error.password}</p>}
+                                    </FormGroup>
+                                    <button className="btn secondary__btn auth__btn" type="submit">Create Account</button>
+                                    <p> Already have any account? <Link to='/login'>Login</Link> </p>
                          </Form>
 
                    </div>
